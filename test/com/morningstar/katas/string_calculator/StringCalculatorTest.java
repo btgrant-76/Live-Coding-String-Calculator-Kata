384c35032a753f483a49cfae2e79c20ab63e87a4
Brian T. Grant <brian.grant@morningstar.com>
2014-02-19 09:48:42 +0530

    Partial implementation with some refactoring around delimiter parsing.

--- test/com/morningstar/katas/string_calculator/StringCalculatorTest.java
+++ test/com/morningstar/katas/string_calculator/StringCalculatorTest.java
@@ -7,47 +7,55 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
-  @Ignore
   @Test
   public void anEmptyStringReturnsZero() {
     assertEquals(0, StringCalculator.calculateString(""));
   }
   
-  @Ignore
   @Test
   public void aSingleNumberReturnsTheValue() {
     assertEquals(5, StringCalculator.calculateString("5"));
   }
   
-  @Ignore
   @Test
   public void twoNumbersCommaDelimitedReturnsTheSum() {
     assertEquals(14, StringCalculator.calculateString("5,9"));
   }
   
-  @Ignore
   @Test
   public void twoNumbersNewlineDelimitedReturnsTheSum() {
     assertEquals(14, StringCalculator.calculateString("5\n9"));
   }
   
-  @Ignore
   @Test
   public void threeNumbersDelimitedEitherWayReturnsTheSum() {
     assertEquals(23, StringCalculator.calculateString("5\n9\n9"));
     assertEquals(23, StringCalculator.calculateString("5,9,9"));
   }
   
-  @Ignore
   @Test(expected=IllegalArgumentException.class)
   public void negativeNumbersThrowAnException() {
     StringCalculator.calculateString("-1");
   }
   
-  @Ignore
   @Test
   public void numbersGreaterThan1000AreIgnored() {
     assertEquals(23, StringCalculator.calculateString("5,9,1025,9"));
+  }
+  
+  @Test
+  public void findCommaDelimiter() {
+    assertEquals(",", StringCalculator.findDelimiter("5,9,1025,9"));
+  }
+  
+  @Test
+  public void findNewLineDelimiter() {
+    assertEquals("\n", StringCalculator.findDelimiter("5\n9\n1025\n9"));
+  }
+  
+  @Test
+  public void aStringWithNoDelimitersYieldsNull() {
+    assertNull(StringCalculator.findDelimiter("5910259"));
   }
   
   @Ignore
