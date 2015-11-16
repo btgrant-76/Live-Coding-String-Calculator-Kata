ccaa66cd03c019b9ccef065510b469f46ac998fd
Brian T. Grant <brian.grant@morningstar.com>
2015-11-16 13:23:16 -0600

    Restored the initial state of the implementation to start from scratch off of master.

--- src/main/java/com/morningstar/katas/string_calculator/StringCalculator.java
+++ src/main/java/com/morningstar/katas/string_calculator/StringCalculator.java
@@ -3,67 +3,6 @@ package com.morningstar.katas.string_calculator;
 public class StringCalculator {
 
   public static int calculateString(String calculateMe) {
-    if (calculateMe.isEmpty()) {
-      return 0;
-    }
-    
-    final String delimiter = findDelimiter(calculateMe);
-    
-    if (delimiter != null) {
-      String[] numbers = calculateMe.split(delimiter);
-      int sum = 0;
-      for (String number : numbers) {
-        int num = Integer.parseInt(number); 
-        if (num >= 1000) {
-          num = 0;
-        }
-        sum += num;
-      }
-      
-      return sum;
-    }
-    
-//    if (calculateMe.indexOf(",") > 0) {
-//      String[] numbers = calculateMe.split(",");
-//      int sum = 0;
-//      for (String number : numbers) {
-//        int num = Integer.parseInt(number); 
-//        if (num >= 1000) {
-//          num = 0;
-//        }
-//        sum += num;
-//      }
-//      return sum;
-//    }
-//    
-//    if (calculateMe.indexOf("\n") > 0) {
-//      String[] numbers = calculateMe.split("\n");
-//      int sum = 0;
-//      for (String number : numbers) {
-//        sum += Integer.parseInt(number);
-//      }
-//      return sum;
-//    }
-    
-    int num = Integer.parseInt(calculateMe); 
-    if (num < 0) {
-      throw new IllegalArgumentException();
-    }
-    
-    return num;
+    return -1;
   }
-  
-  static String findDelimiter(String numbers) {
-    if (numbers.indexOf(",") > 0) {
-      return ",";
-    }
-    
-    if (numbers.indexOf("\n") > 0) {
-      return "\n";
-    }
-    
-    return null;
-  }
-    
-
 }
