4422e1f1a70082ce17482b255f9899fff3b54d72
Brian T. Grant <brian.grant@morningstar.com>
2015-11-16 12:41:38 -0600

    Updated the negative numbers test.

--- src/test/java/com/morningstar/katas/string_calculator/StringCalculatorTest.java
+++ src/test/java/com/morningstar/katas/string_calculator/StringCalculatorTest.java
@@ -1,9 +1,12 @@
 package com.morningstar.katas.string_calculator;
 
-import static org.junit.Assert.*;
-
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class StringCalculatorTest {
 
@@ -12,38 +15,47 @@ public class StringCalculatorTest {
   public void anEmptyStringReturnsZero() {
     assertEquals(0, StringCalculator.calculateString(""));
   }
-  
+
   @Ignore
   @Test
   public void aSingleNumberReturnsTheValue() {
     assertEquals(5, StringCalculator.calculateString("5"));
   }
-  
+
   @Ignore
   @Test
   public void twoNumbersCommaDelimitedReturnsTheSum() {
     assertEquals(14, StringCalculator.calculateString("5,9"));
   }
-  
+
   @Ignore
   @Test
   public void twoNumbersNewlineDelimitedReturnsTheSum() {
     assertEquals(14, StringCalculator.calculateString("5\n9"));
   }
-  
+
   @Ignore
   @Test
   public void threeNumbersDelimitedEitherWayReturnsTheSum() {
     assertEquals(23, StringCalculator.calculateString("5\n9\n9"));
     assertEquals(23, StringCalculator.calculateString("5,9,9"));
   }
-  
+
   @Ignore
-  @Test(expected=IllegalArgumentException.class)
+  @Test
   public void negativeNumbersThrowAnException() {
-    StringCalculator.calculateString("-1");
+    Arrays.asList("-1", "5,-1,9")
+        .stream()
+        .forEach((numString) -> {
+          try {
+            StringCalculator.calculateString(numString);
+            fail("an exception should have been thrown for " + numString);
+          } catch (IllegalArgumentException e) {
+            assertEquals("negative numbers aren't allowed:  -1", e.getMessage());
+          }
+        });
   }
-  
+
   @Ignore
   @Test
   public void numbersGreaterThan1000AreIgnored() {
